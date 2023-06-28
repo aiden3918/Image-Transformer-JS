5fc08e7a83c7dc1aadc735ed6c2e921eb69f3e45
liaiden3918 <liaiden3918@gmail.com>
2023-06-27 21:24:18 -0700

    added basic features except pixelation algo

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ updatePixelCountDisplay = (e) => pixelCountDisplay.innerText = String(e.target.v
 
 // display user-imported image
 function displayImageFile(e) {
+    image.style.width = "auto";
+    image.style.height = "auto";
+
     console.log(e);
     // create url blob to display as image source
     let url = URL.createObjectURL(e.target.files[0]);
@@ -59,7 +62,7 @@ function displayImageFile(e) {
 // create new image on generate-image-btn click
 generateImageBtn.onclick = () => generateImage(pixelCountSlider.value);
 
-function generateImage(pixelCount) {
+function generateImage(pixelCountWidth) {
     // get image data
     context.drawImage(image, 0, 0);
     let imageDataArr = context.getImageData(0, 0, image.width, image.height).data;
@@ -69,31 +72,59 @@ function generateImage(pixelCount) {
     // resize canvas
     let width = image.width;
     let height = image.height;
+
     console.log("width: " + width);
     console.log("height: " + height);
 
     canvas.width = width;
     canvas.height = height;
+    context.clearRect(0, 0, width, height);
 
-    let pixelSize = Math.floor(canvas.width / pixelCount);
-    console.log(pixelSize);
+    let pixelSize = Math.floor(canvas.width / pixelCountWidth);
+    console.log("pixelSize: " + pixelSize);
 
     // maybe round pixel rgb values before averaging? well see how it goes first
     // let roundedimageData = iamgeDataArr.map(e => )
     
     // "iterate" through all sections that will be simplified to one pixel
-    let testheight = 0;
-    let testwidth = 0;
-    for (let i = 0; i < Math.floor(pixelCount * height / width); i++) {
-        testheight++;
+    // # of iterations vertically (height) calculated using aspect ratio
+    for (let i = 0; i < Math.floor(pixelCountWidth * height / width); i++) {
+        for (let j = 0; j < pixelCountWidth; j++) {
+            let currentPixelAvgRGB = calculateAverageRGBValues(imageDataArr, j, i, pixelSize, pixelSize * pixelCountWidth);
+            context.fillStyle = "rgba(" + currentPixelAvgRGB.toString() + ",255)";
+            context.fillRect(j * pixelSize, i * pixelSize, pixelSize, pixelSize);
+            console.log(currentPixelAvgRGB);
+        }
     }
-    for (let j = 0; j < pixelSize; j++) {
-        testwidth++;
+}
+
+// average all rgba values for each "pixel"
+// returns an array of the 
+function calculateAverageRGBValues(imageDataAsArray, startingXPixel, startingYPixel, pixelSize, width) {
+    let sum = 0;
+    let avg = [0, 0, 0];
+
+    let startingXIndexCorrelation = 4 * startingXPixel * pixelSize;
+    let startingYIndexCorrelation = 4 * startingYPixel * width; 
+
+    // repeat this for r, g, and b values
+    for (let i = 0 ; i < 3; i++) {
+        sum = 0;
+        // j represents the current row of the current cell
+        for (let j = 0; j < pixelSize; j++) { 
+            // k represents the current "column" of the current cell (skips 4 each time because array presents all image data as 1D array [r1, g1, b1, a1, r2, g2, b2, a2...]);
+            // skip 4 to get add either r, g, b, or a value every time
+            for (let k = 0; k < pixelSize; k++) { // row
+                sum += imageDataAsArray[startingXIndexCorrelation + startingYIndexCorrelation + i + (4*k) + (4*width*j)]; //SOMETHING TO DO WITH STARTX AND STARTY
+            }
+        }
+        // avg out and put it in single array
+        avg[i] = Math.round(sum / Math.pow(pixelSize, 2));
     }
 
-    console.log("testheight: " + testheight);
-    console.log("testwidth: " + testwidth);
+    return avg;
 }
+
 
 /*function displayCanvasImage(url) {
     return new Promise (resolve => {
